7334de7649e27008de6c0ba9345c01ac504ecbd4
RUPIKASRI <101034066+RUPIKASRI@users.noreply.github.com>
2022-05-14 11:08:06 +0530

    Update wcs1.h

---
+++ 3_Implementation/Inc/wcs1.h
@@ -0,0 +1,20 @@
+*
+ * wcs1.h
+ *
+ *  Created on: May 14, 2022
+ *      Author: Rupika
+ */
+
+#ifndef WCS1_H_
+#define WCS1_H_
+
+void turnoff_wiper(void);
+void turnon_wiper(void);
+void mainkey_off(void);
+void mainkey_on(void);
+void Delay_ms(void);
+void motion_direction_clockwise(void);
+void motion_direction_anticlockwise(void);
+void Reset_Handler(void);
+
+#endif /* WCS1_H_ */
